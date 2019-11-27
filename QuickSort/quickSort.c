d089980c5e0057098fa42603c0b943cd84d8e15d
Victor Silva <victor.silva452@outlook.com>
2019-11-27 01:14:51 -0300

    finished the quick sort and started the optimization

--- QuickSort/quickSort.c
+++ QuickSort/quickSort.c
@@ -1,47 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdbool.h>
 #include "../Utils/utils.h"
 
-// Função para realizar a troca dos valores
+int partition (int *arr, int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
 
-/* This function takes last element as pivot, places 
-the pivot element at its correct position in sorted 
-	array, and places all smaller (smaller than pivot) 
-to left of pivot and all greater elements to right 
-of pivot */
-int partition (int arr[], int low, int high) 
-{ 
-	int pivot = arr[rand () % (low - high)]; // random pivot 
-	int i = (low - 1); // Index of smaller element 
-
-	for (int j = low; j <= high- 1; j++) 
-	{ 
-		// If current element is smaller than the pivot 
+	for (int j = low; j <= high - 1; j++)
+	{
 		if (arr[j] < pivot) 
 		{ 
-			i++; // increment index of smaller element 
-			swap(&arr[i], &arr[j]); 
-		} 
+			i++;
+            swap(&arr[i], &arr[j]);
+        }
 	} 
-	swap(&arr[i + 1], &arr[high]); 
-	return (i + 1); 
-} 
+	swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
 
-/* The main function that implements QuickSort 
-arr[] --> Array to be sorted, 
-low --> Starting index, 
-high --> Ending index */
-void quickSort(int arr[], int low, int high) 
-{ 
-	if (low < high) 
-	{ 
-		/* pi is partitioning index, arr[p] is now 
-		at right place */
-		int pi = partition(arr, low, high); 
+void quick_sort(int *arr, int low, int high)
+{
+    if (low <= high)
+    {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
 
-		// Separately sort elements before 
-		// partition and after partition 
-		quickSort(arr, low, pi - 1); 
-		quickSort(arr, pi + 1, high); 
-	} 
+void quick_sort_optimized(int *arr, int low, int high)
+{
+    if (low <= high)
+    {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
 }
---
+++ QuickSort/quickSort.h
@@ -0,0 +1,12 @@
+//
+// Created by victor on 24/11/2019.
+//
+
+#ifndef UPDATINGALGORITHM_QUICKSORT_H
+#define UPDATINGALGORITHM_QUICKSORT_H
+
+void quick_sort(int *arr, int low, int high);
+
+void quick_sort_optimized(int *arr, int low, int high);
+
+#endif //UPDATINGALGORITHM_QUICKSORT_H
